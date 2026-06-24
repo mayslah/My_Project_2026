fb39bee516d9b3c7f9beece996da2bf2c24eec3a
mayslah <159645930+mayslah@users.noreply.github.com>
2026-06-24 20:09:18 +0300

    Remove unused travelers.h include and related function

--- src/graph_io.h
+++ src/graph_io.h
@@ -2,12 +2,8 @@
 #define GRAPH_IO_H
 
 #include "graph.h"
-#include "travelers.h"
 
-// Milestones 1-3: single src/dst on last line
+// Function to handle file reading
 int read_graph_from_file(const char* filename, Graph* g, int* start_node, int* end_node);
-
-// Milestones 4-6: graph + "# travelers" section
-int read_graph_with_travelers(const char* filename, Graph* g, Traveler* travelers, int* num_travelers);
 
 #endif
