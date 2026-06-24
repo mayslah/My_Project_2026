46b18a32e89ddb8108dd8b8203cb9d9ac12af5da
mayslah <159645930+mayslah@users.noreply.github.com>
2026-06-24 20:08:08 +0300

    Update comments and function declarations in dijkstra.h

--- src/dijkstra.h
+++ src/dijkstra.h
@@ -3,11 +3,8 @@
 
 #include "graph.h"
 
-// Milestone 1: prints path to terminal, also fills final_path globals
+// Milestone 1 function
 void find_shortest_path(Graph* g, int start, int end);
-
-// Milestones 4-6: returns path length (0 = no path), fills path_out[0..n-1]
-int compute_path(Graph* g, int start, int end, int* path_out);
 
 // Milestone 3: Shared variables for the GUI animation
 extern int final_path[MAX_NODES];
